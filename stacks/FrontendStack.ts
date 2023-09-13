6eb44ee3bab84aac9843f7e73824e49f52b3be5a
Hamad Alkhal <alkhamad@amazon.com>
2023-09-13 17:38:57 +0300

    Add reroute for api to avoid cors

--- stacks/FrontendStack.ts
+++ stacks/FrontendStack.ts
@@ -1,9 +1,18 @@
+import { Fn } from "aws-cdk-lib";
+import {
+  AllowedMethods,
+  OriginProtocolPolicy,
+  OriginSslPolicy,
+  ViewerProtocolPolicy,
+} from "aws-cdk-lib/aws-cloudfront";
+import { HttpOrigin } from "aws-cdk-lib/aws-cloudfront-origins";
+
 import { StaticSite, StackContext, use } from "sst/constructs";
 import { ApiStack } from "./ApiStack";
 
 export function FrontendStack({ stack }: StackContext) {
 
-  const {api} = use(ApiStack);
+  const {api, apiCachePolicy} = use(ApiStack);
   
   // Deploy our React app
   const site = new StaticSite(stack, "ReactSite", {
@@ -13,6 +22,24 @@ export function FrontendStack({ stack }: StackContext) {
     environment: {
       REACT_APP_API_URL: api.url,
     },
+    cdk: {
+      distribution: {
+        additionalBehaviors: {
+          "/api/*": {
+            origin: new HttpOrigin(Fn.parseDomainName(api.url), {
+              originSslProtocols: [OriginSslPolicy.TLS_V1_2],
+              protocolPolicy: OriginProtocolPolicy.HTTPS_ONLY,
+            }),
+            viewerProtocolPolicy: ViewerProtocolPolicy.HTTPS_ONLY,
+            cachePolicy: {
+              cachePolicyId: apiCachePolicy.cachePolicyId,
+            },
+            allowedMethods: AllowedMethods.ALLOW_ALL,
+            cachedMethods: AllowedMethods.ALLOW_GET_HEAD_OPTIONS,
+          },
+        },
+      },
+    }
   });
 
   // Show the URLs in the output
